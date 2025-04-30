979b0594561643f3043c2c28865b9a76d3c6ab8c
pujitha-reddy-gth <pujithathammaneni@gmail.com>
2025-04-30 18:27:59 +0530

    Add files via upload

---
+++ Calculators.java
@@ -0,0 +1,12 @@
+public class Calculators{
+public static int add(int a , int b){
+return a + b;
+}
+public static int subtract(int a , int b){
+return a - b;
+}
+public static void main(String args[]){
+System.out.println(Calculators.add(10,5));
+System.out.println(Calculators.subtract(10,5));
+}
+}
